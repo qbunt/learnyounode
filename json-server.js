34d6ce984e3bb84a897403c9ff0a75965141d712
Jeremy Bunting <buntingj@gmail.com>
2016-09-11 20:46:14 -0700

    adding last exercise

--- json-server.js
+++ json-server.js
@@ -1,16 +1,38 @@
 var http = require('http');
 var port = process.argv[2];
 var url = require('url');
-var server = http.createServer((req, res)=>{
-    console.log(req.url)
 
-    switch (req.url){
-        case '':
+var server = http.createServer((req, res)=>{
+    // parse the date object out of the URL
+    var parsedDateObj = url.parse(req.url, true);
+
+    // write a new Date into myDate
+    var myDate = new Date(parsedDateObj.query.iso);
+
+    switch (parsedDateObj.pathname){
+        case '/api/parsetime':
+            var results = {
+                hour: myDate.getHours(),
+                minute: myDate.getMinutes(),
+                second: myDate.getSeconds()
+            };
+        break;
+        case '/api/unixtime':
+            var results = {
+                unixtime: myDate.getTime()
+            };
         break;
     }
 
-    if(req.method == 'get'){
+    // check for method and results, otherwise return 404
+    if(req.method === 'GET' && results !== undefined){
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(results))
+    } else {
+        res.writeHead(404);
+        res.end();
     }
+
 });
 
 server.listen(port);
